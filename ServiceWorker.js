440599efd543f16caf74f9e1a36f3b2d4be58925
Yong Han <230590R@gmail.com>
2025-08-18 00:31:58 +0800

    mew

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,9 +1,9 @@
 const cacheName = "DefaultCompany-CareerDashAlpha-1.0.2";
 const contentToCache = [
     "Build/Build.loader.js",
-    "Build/Build.framework.js.br",
-    "Build/Build.data.br",
-    "Build/Build.wasm.br",
+    "Build/Build.framework.js",
+    "Build/Build.data",
+    "Build/Build.wasm",
     "TemplateData/style.css"
 
 ];
